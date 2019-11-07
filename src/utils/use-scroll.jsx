4daf77a346d454a7f2037c92728f42bf6cc9ddd8
Qiao Lin <qiaoclin@gmail.com>
2019-11-07 23:57:29 +0100

    use e

--- src/utils/use-scroll.jsx
+++ src/utils/use-scroll.jsx
@@ -3,16 +3,16 @@ import { useState, useEffect } from "react";
 export const useScroll = () => {
   // Set a single object `{ x: ..., y: ..., direction: ... }` once on init
   const [scroll, setScroll] = useState({
-    x: window.scrollX,
-    y: window.scrollY,
+    x: 0,
+    y: 0,
     direction: ""
   });
 
   const listener = e => {
     // `prev` provides us the previous state: https://reactjs.org/docs/hooks-reference.html#functional-updates
     setScroll(prev => ({
-      x: window.scrollX,
-      y: window.scrollY,
+      x: e.srcElement.body.scrollX,
+      y: e.srcElement.body.scrollY,
       direction: prev.y > window.scrollY ? "up" : "down"
     }));
   };
